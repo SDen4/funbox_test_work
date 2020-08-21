b338a537a3665cd2a295ed0816ad04acb5fd8ce3
SDen4 <asw325@yandex.ru>
2020-08-21 23:43:16 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# FunBox test work
+## by Denis Skryabin
+## for Junior Frontend Developer (HTML/CSS/JS)
+
+##  Template: react, pug, scss
+
+##  Run development mode:
+```sh
+$ git clone https://github.com/SDen4/funbox_test.git/
+$ cd funbox_test
+$ npm i
+$ npm run dev
+```
+## Watch on [github-pages](https://sden4.github.io/funbox_test/)
---
+++ src/components/app.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+
+    render() {
+        return (
+            <div className="app">
+                <h1>Title of the APP</h1>
+                <img className="image" srcSet="assets/images/pic.jpg"></img>
+            </div>
+        );
+    }
+};
+
+export default App;
