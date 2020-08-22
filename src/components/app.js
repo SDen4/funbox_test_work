0d89a28ccef2fe885df7cebd531a9d8de99efb63
SDen4 <asw325@yandex.ru>
2020-08-22 13:06:56 +0500

    fonts

--- src/components/app.js
+++ src/components/app.js
@@ -5,11 +5,14 @@ class App extends Component {
     render() {
         return (
             <div className="app">
-                <h1>Title of the APP</h1>
+                <h1 className="title">Ты сегодня покормил кота?</h1>
+                <h1 className="test">Нямушка</h1>
+                <h1 className="test2">Сказочное заморское яство</h1>
+                <h1 className="test3">Сказочное заморское яство</h1>
                 <img className="image" srcSet="assets/images/pic.jpg"></img>
             </div>
         );
     }
 };
 
-export default App;
+export default App; 
