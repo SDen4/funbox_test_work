5a250dbb6f0407e27ebdb8da4699a53adacb2d65
SDen4 <asw325@yandex.ru>
2020-08-23 18:58:53 +0500

    auto list

--- src/components/app.js
+++ src/components/app.js
@@ -3,26 +3,18 @@ import Item  from "./item";
 import items from "../scripts/itemsList";
 
 class App extends Component {
-    state = {
-        items: []
-    }
-
     render() {
+        let itemsList = items.map( function (item) {
+            return <li key={item.id}> <Item item={item} /> </li>
+        });
         return (
             <div className="app">
                 <h1 className="title">Ты сегодня покормил кота?</h1>
-                <main className="main">
-                    <Item item={items[0]} />
-                    <Item item={items[1]} />
-                    <Item item={items[2]} />
-                </main>
+                <ul className="main">
+                    {itemsList}
+                </ul>
             </div>
         );
-    }
-    getElements() {
-        this.setState({
-            items: items
-        })
     }
 };
 
